f6eb0376bb05bd63dae24951b71e18daa0c11d85
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-10-02 10:49:13 +0900

    delete: 不要なdiv

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -20,17 +20,15 @@ const boxStyle = {
 
 export const ImageModal: FC<Props> = ({ imageName, modalOpen, handleClose }) => {
 	return (
-		<div>
-			<Modal
-				open={modalOpen}
-				onClose={handleClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description"
-			>
-				<Box sx={boxStyle}>
-					<img src={`/image/ticket/${imageName}`} style={{ maxHeight: '90vh', maxWidth: '90vw' }} />
-				</Box>
-			</Modal>
-		</div>
+		<Modal
+			open={modalOpen}
+			onClose={handleClose}
+			aria-labelledby="modal-modal-title"
+			aria-describedby="modal-modal-description"
+		>
+			<Box sx={boxStyle}>
+				<img src={`/image/ticket/${imageName}`} style={{ maxHeight: '90vh', maxWidth: '90vw' }} />
+			</Box>
+		</Modal>
 	);
 };
