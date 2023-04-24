058d5d642a057d2dd1e5f83eb6e84c6143173318
takonasu <piano88_viola_tako8@hotmail.co.jp>
2023-04-24 23:18:36 +0900

    add: 日付降順

--- src/App.tsx
+++ src/App.tsx
@@ -16,37 +16,39 @@ const App = () => {
 				<Stack spacing={2}>
 					<Typography>参加ライブ数：{liveHistory.length}公演</Typography>
 
-					{liveHistory.map((live) => {
-						const [modalOpen, setModalOpen] = React.useState(false);
-						const handleClose = () => {
-							setModalOpen(false);
-						};
-						return (
-							<>
-								{live.ticketImg ? (
-									<ImageModal imageName={live.ticketImg} modalOpen={modalOpen} handleClose={handleClose} />
-								) : null}
-								<Card
-									variant="outlined"
-									key={live.day}
-									onClick={() => {
-										if (live.ticketImg) {
-											setModalOpen(true);
-										}
-									}}
-								>
-									<CardContent>
-										<Typography>{format(new Date(live.day), 'yyyy年MM月dd日')}</Typography>
-										<Typography>{live.name}</Typography>
-										<Typography>会場：{live.place}</Typography>
-										<Typography>
-											<a href={live.link}>外部リンク</a>
-										</Typography>
-									</CardContent>
-								</Card>
-							</>
-						);
-					})}
+					{liveHistory
+						.sort((a, b) => (a.day < b.day ? 1 : -1))
+						.map((live) => {
+							const [modalOpen, setModalOpen] = React.useState(false);
+							const handleClose = () => {
+								setModalOpen(false);
+							};
+							return (
+								<>
+									{live.ticketImg ? (
+										<ImageModal imageName={live.ticketImg} modalOpen={modalOpen} handleClose={handleClose} />
+									) : null}
+									<Card
+										variant="outlined"
+										key={live.day}
+										onClick={() => {
+											if (live.ticketImg) {
+												setModalOpen(true);
+											}
+										}}
+									>
+										<CardContent>
+											<Typography>{format(new Date(live.day), 'yyyy年MM月dd日')}</Typography>
+											<Typography>{live.name}</Typography>
+											<Typography>会場：{live.place}</Typography>
+											<Typography>
+												<a href={live.link}>外部リンク</a>
+											</Typography>
+										</CardContent>
+									</Card>
+								</>
+							);
+						})}
 				</Stack>
 			</Container>
 		</div>
